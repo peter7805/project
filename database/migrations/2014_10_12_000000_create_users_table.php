706c523ff8b0edb2b6df0227ae707287bf7b07fe
peter7805 <yiping7805@gmail.com>
2020-10-30 10:50:57 +0800

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('account', 50);
+            $table->string('userId', 50);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->integer('balance')->nullable();
+            $table->integer('login_failed')->nullable();;
+            $table->timestamp('login_time')->nullable();
+            $table->foreignId('current_team_id')->nullable();
+            $table->text('profile_photo_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2020_10_30_014454_create_account_info_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accountInfo', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->bigInteger('number');
+            $table->double('amount', 10, 2);
+            $table->double('money', 10, 2);
+            $table->double('balance', 10, 2);
+            $table->integer('type');
+            $table->string('remark', 320)->nullable();
+            $table->bigInteger('time')->nullable();
+            $table->timestamp('create_time')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accountInfo');
+    }
+}
